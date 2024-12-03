5d8b1acee6863d7b708e0717f45dbef6e307feea
Zachary Lee <mrzclee@gmail.com>
2024-12-02 23:04:12 -0500

    removed py lsp and metals

--- init.lua
+++ init.lua
@@ -536,7 +536,6 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
